52b0b988e07ce3f117b24b7bf90d3b3956733c1f
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-29 22:44:44 -0400

    added rendering states and fetching of profile info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getUserInfo } from '../../utilities/users-service'
 
-export default function ProfilePage()
+
+export default function ProfilePage({ loggedInUser })
 {
+    // TODO: fetch profile page data and conditionally render different portions based on whether it matches the currently logged in user
+    const [profile, setProfile] = useState(null)
+    const { userId } = useParams()
+
+    useEffect(() =>
+    {
+        async function fetchProfileInfo()
+        {
+            const thisUserProfile = await getUserInfo(userId)
+            console.log('FETCHING PROFILE INFO')
+            console.log(thisUserProfile)
+            setProfile(thisUserProfile)
+        }
+        fetchProfileInfo()
+    }, [userId])
+
+    const userProfile = (
+        <h2>{profile?.user?.name}</h2>
+    )
+
+    const loader = <h2>Loading...</h2>
+
     return (
-        <div>ProfilePage</div>
+        <div>
+            <h1>Profile Page</h1>
+            {
+                profile ? userProfile : loader
+            }
+        </div>
     )
 }
