23675df82ff6e705735460adbc4e4b1b40e0a3b8
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-03 15:41:17 -0400

    added starter styling for User component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom'
+import styles from './User.module.css'
 
 export default function User({ user, isCompact })
 {
     const content = (
         <>
             <Link to={`/users/${user._id}`}>
-                <img src={user.profile.profileImgUrl}
+                <img className={`${styles.userImg} ${isCompact ? styles.compact : styles.normal}`}
+                    src={user.profile.profileImgUrl}
                     alt={`${user.name} profile`}
                     title={`${user.name}`} />
             </Link>
-            {!isCompact && 'INFO'}
+            {
+                !isCompact &&
+                <h3>{user.name}</h3>
+            }
         </>
     )
 
